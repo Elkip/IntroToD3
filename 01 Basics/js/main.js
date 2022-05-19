b1d0d4855ad127c81cae064f317a210943e4a202
elkip <mitchiscool51@gmail.com>
2022-05-19 09:00:10 -0400

    Data Binding

--- 01 Basics/js/main.js
+++ 01 Basics/js/main.js
@@ -1,22 +1,15 @@
+
+// Set up SVG grid
 const svg = d3.select("#chart-area").append("svg")
     .attr("width", 400)
     .attr("height", 400)
 
-/*
-svg.append("circle")
-    .attr("cx", 100)
-    .attr("cy", 250)
-    .attr("r", 70)
-    .attr("fill", "red")
-*/
-
-/*svg.append("rect")
+svg.append("rect")
     .attr("x", 10)
     .attr("y", 25)
-    .attr("width", 400)
-    .attr("height", 400)
-    .attr("fill", "blue")*/
-
+    .attr("width", 10)
+    .attr("height", 40)
+    .attr("fill", "blue")
 
 svg.append("line")
     .attr("x1", 10)
@@ -25,3 +18,24 @@ svg.append("line")
     .attr("y2", 400)
     .attr("stroke", "blue")
     .attr("stroke-fill", "5")
+
+const data = [25, 20, 15, 10, 5]
+
+// Select all circles on screen and associate with our data
+const circles = svg.selectAll("circle")
+    .data(data)
+
+// Using the enter method we can append the data,
+// so we have a circle for every item in the data array
+// Doing this we can also set the attributes to be
+// a function of data in the array
+circles.enter().append("circle")
+    .attr("cx", (d, i) => {
+        console.log("Item: " + d, "Index: " + i)
+        return (i * 50) + 50
+    })
+    .attr("cy", 250)
+    .attr("r", (d) => d)
+    .attr("fill", "red")
+
+
