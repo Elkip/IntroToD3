2e71076b60d7ead7beb2e641c790a5de4186d8e1
elkip <mitchiscool51@gmail.com>
2022-05-20 08:49:34 -0400

    Loading data and error handling

--- 01 Basics/js/main.js
+++ 01 Basics/js/main.js
@@ -1,15 +1,7 @@
-
 // Set up SVG grid
 const svg = d3.select("#chart-area").append("svg")
-    .attr("width", 400)
+    .attr("width", 800)
     .attr("height", 400)
-
-svg.append("rect")
-    .attr("x", 10)
-    .attr("y", 25)
-    .attr("width", 10)
-    .attr("height", 40)
-    .attr("fill", "blue")
 
 svg.append("line")
     .attr("x1", 10)
@@ -38,4 +30,23 @@ circles.enter().append("circle")
     .attr("r", (d) => d)
     .attr("fill", "red")
 
+// Load data from file
+d3.json("data/buildings.json").then(data => {
+    console.log(data)
+    data.forEach(d => {
+        // Set height to be a number instead of a string
+        d.height = Number(d.height)
+    })
 
+const rectangles = svg.selectAll("rect")
+        .data(data)
+
+rectangles.enter().append("rect")
+    .attr("y", 0)
+    .attr("x", (d,i) => (i * 30) + 250)
+    .attr("width", 20)
+    .attr("height", d => d.height)
+    .attr("fill", "blue")
+}).catch(error => {
+    console.log(error)
+})
