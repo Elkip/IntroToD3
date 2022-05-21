ecd6f0b3d818842445b2ea11208a8bb5112a503c
elkip <mitchiscool51@gmail.com>
2022-05-21 13:30:45 -0400

    Graphing and scaling

---
+++ 02 Graphing/js/main.js
@@ -0,0 +1,27 @@
+const svg = d3.select("#chart-area").append("svg")
+    .attr("width", 400)
+    .attr("height", 400)
+
+d3.json("data/buildings.json").then(data => {
+    console.log(data)
+    data.forEach(d => {
+        d.height = Number(d.height)
+    })
+
+    // Scaling
+    const y = d3.scaleLinear()
+        .domain([0, 828])
+        .range([0, 400])
+
+    const rectangles = svg.selectAll("rect")
+        .data(data)
+
+    rectangles.enter().append("rect")
+        .attr("y", 0)
+        .attr("x", (d,i) => (i * 60))
+        .attr("width", 40)
+        .attr("height", d => y(d.height))
+        .attr("fill", "blue")
+}).catch(error => {
+    console.log(error)
+})
