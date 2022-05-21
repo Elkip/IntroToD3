c92cb3bf509f1d37f0e4a4608d8ad96050a4df01
elkip <mhensche@emich.edu>
2022-05-21 16:07:34 -0400

    Band Scaling

--- 02 Graphing/js/main.js
+++ 02 Graphing/js/main.js
@@ -9,6 +9,15 @@ d3.json("data/buildings.json").then(data => {
     })
 
     // Scaling
+    const x = d3.scaleBand()
+        .domain(["Burj Khalifa", "Shanghai Tower",
+            "Abraj Al-Bait Clock Tower", "Ping An Finance Centre",
+            "Lotte World Tower", "One World Trade Center",
+            "CTF Finance Centre"])
+        .range([0, 400])
+        .paddingInner(0.3)
+        .paddingOuter(0.2)
+
     const y = d3.scaleLinear()
         .domain([0, 828])
         .range([0, 400])
@@ -18,8 +27,8 @@ d3.json("data/buildings.json").then(data => {
 
     rectangles.enter().append("rect")
         .attr("y", 0)
-        .attr("x", (d,i) => (i * 60))
-        .attr("width", 40)
+        .attr("x", (d) => x(d.name))
+        .attr("width", x.bandwidth())
         .attr("height", d => y(d.height))
         .attr("fill", "blue")
 }).catch(error => {
