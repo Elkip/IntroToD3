b66e014a2f616d5f3093cfb3314d840ca5be0472
elkip <mitchiscool51@gmail.com>
2022-07-05 22:39:25 -0400

    Axes and Labels

--- 02 Graphing/js/main.js
+++ 02 Graphing/js/main.js
@@ -1,4 +1,4 @@
-const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
+const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 130 }
 const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
 
@@ -6,10 +6,29 @@ const svg = d3.select("#chart-area").append("svg")
     .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
     .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
-// Append a group element
+// Append a group element and shift visualization to middle of page
 const g = svg.append("g")
-    // Shift visualization to middle of page
-    .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP}`)
+    .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+
+// X Label
+g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", WIDTH / 2)
+    .attr("y", HEIGHT + 110)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("The word's tallest buildings")
+
+// Y label
+g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", - (HEIGHT / 2))
+    .attr("y", -50)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Height (m)")
+
 
 d3.json("data/buildings.json").then(data => {
     console.log(data)
@@ -19,23 +38,44 @@ d3.json("data/buildings.json").then(data => {
 
     // Scaling
     const x = d3.scaleBand()
-        .domain(data.map(d => d.name) )
+        .domain(data.map(d => d.name))
         .range([0, WIDTH])
         .paddingInner(0.3)
         .paddingOuter(0.2)
 
     const y = d3.scaleLinear()
         .domain([0, d3.max(data, d => d.height)])
-        .range([0, HEIGHT])
+        .range([HEIGHT, 0])
+
+    // X Axis
+    const xAxisCall = d3.axisBottom(x)
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", `translate(0, ${HEIGHT})`)
+        .call(xAxisCall)
+        // Apply transformation to make text readable
+        .selectAll("text")
+            .attr("y", "10")
+            .attr("x", "-5")
+            .attr("text-anchor", "end")
+            .attr("transform", "rotate(-40)")
+
+    // Y Axis
+    const yAxisCall = d3.axisLeft(y)
+        .ticks(3)
+        .tickFormat(d => d + "m")
+    g.append("g")
+        .attr("class", "y axis")
+        .call(yAxisCall)
 
     const rectangles = g.selectAll("rect")
         .data(data)
 
     rectangles.enter().append("rect")
-        .attr("y", 0)
+        .attr("y", d => y(d.height))
         .attr("x", (d) => x(d.name))
-        .attr("width", x.bandwidth())
-        .attr("height", d => y(d.height))
+        .attr("width", x.bandwidth)
+        .attr("height", d => HEIGHT - y(d.height))
         .attr("fill", "blue")
 }).catch(error => {
     console.log(error)
