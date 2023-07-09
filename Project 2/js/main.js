593f628bcf80178c3d10d82ae640d20ed0d42f5c
elkip <mitchiscool51@gmail.com>
2023-07-09 12:02:31 -0400

    Project 2 Functional

--- Project 2/js/main.js
+++ Project 2/js/main.js
@@ -5,8 +5,8 @@
 */
 
 const MARGIN = { LEFT: 100, RIGHT: 10, TOP: 10, BOTTOM: 100 }
-const WIDTH = 600 - MARGIN.LEFT - MARGIN.RIGHT
-const HEIGHT = 400 - MARGIN.TOP - MARGIN.BOTTOM
+const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
+const HEIGHT = 500 - MARGIN.TOP - MARGIN.BOTTOM
 
 const svg = d3.select("#chart-area").append("svg")
 	.attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
@@ -16,8 +16,7 @@ const g = svg.append("g")
 	.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
 
 // X Label
-g.append("text")
-	.attr("class", "x axis-label")
+const xLabel = g.append("text")
 	.attr("x", WIDTH / 2)
 	.attr("y", HEIGHT + 100)
 	.attr("font-size", "20px")
@@ -25,25 +24,68 @@ g.append("text")
 	.text("GDP Per Capita")
 
 // Y Label
-g.append("text")
-	.attr("class", "y axis-label")
-	.attr("x", WIDTH / 2)
-	.attr("y", -100)
+const yLabel = g.append("text")
+	.attr("transform", "rotate(-90)")
+	.attr("y", -70)
+	.attr("x", -170)
 	.attr("font-size", "20px")
 	.attr("text-anchor", "middle")
 	.text("Avg. Life Expectancy")
 
+// Time Label
+const timeLabel = g.append("text")
+	.attr("y", HEIGHT - 10)
+	.attr("x", WIDTH - 40)
+	.text("1800")
+
+// Scaling
+const x = d3.scaleLog()
+	.domain([142, 150000])
+	.range([0, WIDTH])
+const y = d3.scaleLinear()
+	// .domain([0, d3.max(data, d => d.life_exp)])
+	.domain([0, 100])
+	.range([HEIGHT, 0])
+const area = d3.scaleLinear()
+	.range([25*Math.PI, 1500*Math.PI])
+	.domain([2000, 1400000000])
+const contientColor = d3.scaleOrdinal(d3.schemePastel1)
+
+// X Axis
+const xAxisCall = d3.axisBottom(x)
+	.tickValues([400, 4000, 40000])
+	.tickFormat(d => '$' + d)
+g.append("g")
+	.attr("class", "x axis")
+	.attr("transform", `translate(0, ${HEIGHT})`)
+	.call(xAxisCall)
+	.selectAll("text")
+	.attr("y", "10")
+	.attr("x", "-5")
+	.attr("text-anchor", "end")
+	.attr("transform", "rotate(-40)")
+
+// Y Axis
+const yAxisCall = d3.axisLeft(y)
+	.ticks(5)
+	.tickFormat(d => d + " years")
+g.append("g")
+	.attr("class", "y axis")
+	.call(yAxisCall)
+
 d3.json("data/data.json").then(function(rawData){
 //	console.log(rawData);
-
 	const data = rawData.map(d => {
 							return {
 								year: d["year"],
 								countries: d["countries"]
-									.filter(country => (country.income && country.life_exp))
+									.filter(country => (country.income &&
+														country.life_exp &&
+														country.population))
 									.map(country => {
 										country.income = Number(country.income)
 										country.life_exp = Number(country.life_exp)
+										country.population = Number(country.population)
 										return country
 									})
 							}
@@ -52,52 +94,35 @@ d3.json("data/data.json").then(function(rawData){
 
 	console.log(data)
 
-	update(data.find(d => d.year === "1800")["countries"])
+	let currentYear = 1800
+	d3.interval(() => {
+		currentYear = (currentYear > 2013) ?  1800 : currentYear += 1
+		console.log("current year: " + currentYear)
+		update(data.find(d => d.year === currentYear.toString())["countries"], currentYear)
+	}, 100)
+
+	update(data.find(d => d.year === currentYear.toString())["countries"], currentYear)
 
 }).catch(error => {
 	console.log(error)
 })
 
-function update(data) {
+function update(data, year) {
 	console.log(data)
-	// Scaling
-	const x = d3.scaleBand()
-		.domain(data.map(d => d.income))
-		.range([0, WIDTH])
-		.paddingInner(0.3)
-		.paddingOuter(0.2)
-
-	const y = d3.scaleLinear()
-		.domain([0, d3.max(data, d => d.life_exp)])
-		.range([HEIGHT, 0])
-
-	// X Axis
-	const xAxisCall = d3.axisBottom(x)
-	g.append("g")
-		.attr("class", "x axis")
-		.attr("transform", `translate(0, ${HEIGHT})`)
-		.call(xAxisCall)
-		// Apply transformation to make text readable
-		.selectAll("text")
-		.attr("y", "10")
-		.attr("x", "-5")
-		.attr("text-anchor", "end")
-		.attr("transform", "rotate(-40)")
-
-	// Y Axis
-	const yAxisCall = d3.axisLeft(y)
-		.ticks(3)
-		.tickFormat(d => d + " years")
-	g.append("g")
-		.attr("class", "y axis")
-		.call(yAxisCall)
+	const t = d3.transition().duration(100)
 
 	const dots = g.selectAll("circle")
 		.data(data, d => d.country)
 
-	dots.enter().append("rect")
-		.attr("cy", d => y(d.life_exp))
-		.attr("cx", d => x(d.income))
-		.attr("r", 5)
-		.attr("fill", "blue")
+	dots.exit().remove()
+
+	dots.enter().append("circle")
+		.attr("fill", d => contientColor(d.continent))
+		.merge(dots)
+		.transition(t)
+			.attr("cx", d => x(d.income))
+			.attr("cy", d => y(d.life_exp))
+			.attr("r", d => Math.sqrt((area(d.population)/Math.PI)))
+
+	timeLabel.text(String(year))
 }
